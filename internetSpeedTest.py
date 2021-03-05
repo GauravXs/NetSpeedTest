db4a1bc9b82ca42e93d43664b827c797cbe6cfe1
YashTripathi03 <80109678+YashTripathi03@users.noreply.github.com>
2021-03-05 19:37:19 +0530

    Delete internetSpeedTest.py

--- internetSpeedTest.py
+++
@@ -1,9 +0,0 @@
-from speedtest import Speedtest
-
-st = Speedtest()
-download = st.download()
-upload = st.upload()
-download_speed = round(download / (10**6), 2)
-upload_speed = round(upload / (10**6), 2)
-print("Download Speed:" + str(download_speed) + "Mbps")
-print("Upload Speed:" + str(upload_speed) + "Mbps")
